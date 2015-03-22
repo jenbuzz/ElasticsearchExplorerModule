60b6295468ddd8952773aa5d03fba544c3d1dcf3
dan-lyn <daniel_lynge@hotmail.com>
2015-03-22 15:30:58 +0100

    added css and js from elasticsearchexplorerbundle (app specific, foundation, and vendors)

---
+++ public/js/search.js
@@ -0,0 +1,14 @@
+var doRedirectSearch = function(value) {
+  var arrUrl = $(location).attr('href').split('/');
+  if (value!=-1 && value!=arrUrl[arrUrl.length-2]) {
+    document.location+= value;
+  }
+};
+
+$('#search-select-index').on('change', function() {
+  doRedirectSearch($(this).val());
+});
+
+$('#search-select-type').on('change', function() {
+  doRedirectSearch($(this).val());
+});
