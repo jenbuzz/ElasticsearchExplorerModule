b6ba9c013411c3d039ba4722ef2157a4bfb3fd70
dan-lyn <daniel_lynge@hotmail.com>
2015-04-18 14:16:48 +0200

    put search js into document.ready block

--- public/js/search.js
+++ public/js/search.js
@@ -1,14 +1,16 @@
-var doRedirectSearch = function(value) {
-  var arrUrl = $(location).attr('href').split('/');
-  if (value!=-1 && value!=arrUrl[arrUrl.length-2]) {
-    document.location+= value;
-  }
-};
+$(document).ready(function() {
+  var doRedirectSearch = function(value) {
+    var arrUrl = $(location).attr('href').split('/');
+    if (value!=-1 && value!=arrUrl[arrUrl.length-2]) {
+      document.location+= value;
+    }
+  };
 
-$('#search-select-index').on('change', function() {
-  doRedirectSearch($(this).val());
-});
+  $('#search-select-index').on('change', function() {
+    doRedirectSearch($(this).val());
+  });
 
-$('#search-select-type').on('change', function() {
-  doRedirectSearch($(this).val());
+  $('#search-select-type').on('change', function() {
+    doRedirectSearch($(this).val());
+  });
 });
