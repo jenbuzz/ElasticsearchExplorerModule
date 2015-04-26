e222d7713691064e468a6be24801329dedd9ee97
dan-lyn <daniel_lynge@hotmail.com>
2015-04-26 11:25:35 +0200

    setup compass task in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,13 +20,22 @@ module.exports = function(grunt) {
         src: 'public/js/search.js',
         dest: 'public/js/search.min.js'
       }
+    },
+    compass: {
+      dist: {
+        options: {
+          config: 'config.rb'
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
   grunt.registerTask('minify', ['uglify']);
-  grunt.registerTask('compile', ['uglify']);
+  grunt.registerTask('compile', ['uglify', 'compass']);
 
 };
